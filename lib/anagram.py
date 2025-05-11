e61cf61bf3374b80b96faf55ffca29b7a1c67b88
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-05-11 13:34:26 +0300

    python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):  
+        self.word = word
+
+    def match(self, possible_anagrams):
+        normalized_word = sorted(self.word.lower())  
+        matches = []
+
+        for candidate in possible_anagrams:
+            if sorted(candidate.lower()) == normalized_word:
+                matches.append(candidate)
+
+        return matches
+
+
+
+anagram_checker = Anagram('listen')
+possible_anagrams = ['enlists', 'google', 'inlets', 'banana']
+print(anagram_checker.match(possible_anagrams))  
